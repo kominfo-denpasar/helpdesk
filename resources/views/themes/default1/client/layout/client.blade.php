f626464fe080d4d715ab1d01ca50b58679453fa3
zeneight <thezeneight@gmail.com>
2024-03-13 23:54:32 +0800

    feat(front): add tawk.to

--- resources/views/themes/default1/client/layout/client.blade.php
+++ resources/views/themes/default1/client/layout/client.blade.php
@@ -600,6 +600,20 @@ $(function () {
 });
             </script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65f1cb0f8d261e1b5f6d425e/1hos82doc';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
         </div>
         </div>
     </body>
