d5a4632974349f7ebd2353675eba9330c0653ab8
zeneight <thezeneight@gmail.com>
2024-05-03 15:48:21 +0800

    miscs: hide livechat dan force bahsa ke id

--- resources/views/themes/default1/client/layout/client.blade.php
+++ resources/views/themes/default1/client/layout/client.blade.php
@@ -719,7 +719,7 @@
       });
     </script>
     <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
       var Tawk_API = Tawk_API || {},
         Tawk_LoadStart = new Date();
       (function() {
@@ -731,7 +731,7 @@
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
       })();
-    </script>
+    </script> -->
     <!--End of Tawk.to Script-->
 
   </div>
